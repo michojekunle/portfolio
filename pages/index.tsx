e31129875fbb385d436456b23f829469b7f871fb
AMD <michojekunle1@gmail.com>
2023-03-04 09:24:49 +0100

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import Hero from '../components/Hero'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  
   return (
     <div className="bg-[#242424] text-white h-screen snap-y snap-mandatory overflow-scroll ">
       <Head>
