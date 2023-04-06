ec3aae9c5f43e87c61ff764022a1f0b0833bdeae
AMD <michojekunle1@gmail.com>
2023-04-06 22:40:06 +0100

    made changes

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -8,6 +8,7 @@ const Projects = (props: Props) => {
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl text-center'>
         Projects
       </h3>
+      
     </div>
   )
 }
