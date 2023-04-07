dcdd376626aa5053ff399efde30978dcc1f98ebe
AMD <michojekunle1@gmail.com>
2023-04-07 23:04:10 +0100

    made changes

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -8,7 +8,11 @@ const Projects = (props: Props) => {
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl text-center'>
         Projects
       </h3>
-      
+
+      <div className='relative w-full flex overflow-x-scroll overflow-y-hidden '>
+
+      </div>
+      <div className="w-full absolute top-[30px]bg-[#F7ABBA]/10 left-0 h-[500px] -skew-y-12" />
     </div>
   )
 }
