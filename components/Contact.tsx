467fa1bea1c66982af7163bf1a79af27bc8606e8
AMD <michojekunle1@gmail.com>
2023-04-09 21:35:26 +0100

    changes

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { PhoneIcon, MapPinIcon, EnvelopeIcon } from '@heroicons/react/24/solid';
 
 type Props = {}
 
@@ -11,11 +12,33 @@ function Contact({}: Props) {
         <div className="flex flex-col space-y-10">
           <h4 className='text-4xl font-semibold text-center '>
             I havs got just what you need
-            <span className='underline decoration-[#F7ABBA]/50 '>Let's Talk</span>
+            <span className='underline decoration-[#F7ABBA]/50 '>Lets Talk</span>
           </h4>
-          <div className=''>
+          <div className='space-y-10'>
+            <div className='flex items-center space-x-5'>
+              <PhoneIcon className='text-[#F7ABBA] h-7 w-7 animate-pulse'/>
+              <p className='text-2xl'>+234 904 515 6850 </p>
+            </div>
 
+            <div className='flex items-center space-x-5'>
+              <EnvelopeIcon className='text-[#F7ABBA] h-7 w-7 animate-pulse'/>
+              <p className='text-2xl'>michojekunle1@gmail.com</p>
+            </div>
+
+            <div className='flex items-center space-x-5'>
+              <MapPinIcon className='text-[#F7ABBA] h-7 w-7 animate-pulse'/>
+              <p className='text-2xl'>Yaba, Lagos Nigeria.</p>
+            </div>
           </div>
+          <form className='flex flex-col space-y-2 w-fit mx-auto'>
+            <div className='flex space-x-2'>
+              <input className='contactInput' placeholder='Name' type="text" />
+              <input className='contactInput' placeholder='Email' type="email" />
+            </div>
+            <input className='contactInput' placeholder='Subject' type="text" />
+            <textarea placeholder='Message'/>
+            <button type='submit' className='text-lg py-5 px-10 rounded-md text-black font-bold bg-[#F7ABBA]'>Submit</button>
+          </form>
         </div>
     </div>
   )
