80fd00fd4f7665bca128f914f8254c2ed666fc0d
AMD <michojekunle1@gmail.com>
2023-04-26 18:02:42 +0100

    made changes

--- sanity.ts
+++ sanity.ts
@@ -1,9 +1,14 @@
 import { createClient } from 'next-sanity';
 import createImageUrlBuilder from '@sanity/image-url';
 
+interface Config {
+    dataset: string;
+    projectId: any;
+    apiVersion: string;
+    useCdn: boolean; 
+}
 
-
-export const config = {
+export const config: Config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     apiVersion: "2023-04-21",
@@ -13,4 +18,4 @@ export const config = {
 // Set up the client for fetching data in the getProps page features
 export const sanityClient = createClient(config);
 
-export const urlFor = (source) => createImageUrlBuilder(config).image(source);
+export const urlFor = (source: any) => createImageUrlBuilder(config).image(source);
