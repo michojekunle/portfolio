aab3dabef326674216a69a15b257739b7478813f
AMD <michojekunle1@gmail.com>
2023-04-26 01:51:46 +0100

    sanity.ts file editrd

--- sanity.ts
+++ sanity.ts
@@ -1,7 +1,14 @@
 import { createClient } from 'next-sanity';
 import createImageUrlBuilder from '@sanity/image-url';
 
-export const config = {
+interface Config {
+    dataset: string;
+    projectId: string;
+    apiVersion: string;
+    useCdn: boolean;
+}
+
+export const config: Config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     apiVersion: "2023-04-21",
