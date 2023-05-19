87d2ecf349250089f0105b36e6a8f90ef5907577
AMD <michojekunle1@gmail.com>
2023-05-19 09:08:04 +0100

    changes

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -16,9 +16,9 @@ const BackgroundCircles = (props: Props) => {
         transition = {{
             duration: 2.5,
         }}
-        className=' relative flex items-center justify-center'>
-        <div className='absolute border border-[#333333] mt-52 animate-ping rounded-full w-[250px] h-[250px] md:w-[200px] md:h-[200px] '/>
-        <div className='absolute border border-[#333333] mt-52 animate-pulse opacity-100 md:opacity-0 rounded-full w-[280px] h-[280px]'/>
+        className=' relative flex items-center justify-center sm:block hidden'>
+        <div className='absolute border border-[#333333] mt-52 animate-ping rounded-full lg:w-[250px] lg:h-[250px] md:w-[200px] md:h-[200px] sm:'/>
+        <div className='absolute border border-[#333333] mt-52 animate-pulse opacity-100 md:opacity-0 rounded-full w-[180px] h-[180px] md:w-[280px] md:h-[280px] '/>
         <div className='absolute border border-[#333333] mt-52 animate-ping rounded-full w-[300px] h-[300px] opacity-0  md:opacity-100'/>
         <div className='absolute border border-[#333333] mt-52 animate-ping rounded-full w-[500px] h-[500px] opacity-0 md:opacity-100'/>
         <div className='absolute border border-[#333333] mt-52 animate-pulse rounded-full w-[650px] h-[650px] opacity-0 md:opacity-20'/>
