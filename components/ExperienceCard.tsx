910b69ef45281afefc736febc8059e6dd017bb1d
AMD <michojekunle1@gmail.com>
2023-05-31 09:50:56 +0100

    made changes

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -9,13 +9,13 @@ type Props = {
 
 const ExperienceCard = ({ experience }: Props) => {
   return (
-    <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[320px] sm:w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#242424] p-10 opacity-40 hover:opacity-100 cursor-pointer transition-opacity duration-200'>
+    <article className='flex flex-col rounded-md md:rounded-lg items-center space-y-3 md:space-y-7 flex-shrink-0 w-[280px] xs:w-[320px] sm:w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#242424] p-3 md:p-10 opacity-40 hover:opacity-100 cursor-pointer transition-opacity duration-200'>
         <motion.div
           initial={{ y: -200, opacity: 0 }}
           transition={{ duration: 1.2}}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{once: true }}
-          className='relative w-[80px] h-[80px] flex items-center justify-center rounded-full xl:w-[200px] xl:h-[200px]'
+          className='relative w-[40px] h-[40px] md:w-[80px] md:h-[80px] flex items-center justify-center rounded-full xl:w-[200px] xl:h-[200px]'
         >
           <Image
               src ={experience.companyImage}
@@ -23,8 +23,8 @@ const ExperienceCard = ({ experience }: Props) => {
           />
         </motion.div>
         <div className='px-0 md:px-10'>
-          <h4 className='text-4xl'>{experience.jobTitle}</h4>
-          <p className='font-bold text-2xl mt-1'>{experience.company}</p>
+          <h4 className='text-2xl md:text-4xl'>{experience.jobTitle}</h4>
+          <p className='font-bold text-lg md:text-2xl mt-1'>{experience.company}</p>
           <div className='flex space-x-5 my-5'>
             {experience.technologies.map((technology, i) => (
               <Image
@@ -40,7 +40,7 @@ const ExperienceCard = ({ experience }: Props) => {
             {new Date(experience.dateStarted).toDateString()} - {" "} {experience.isCurrentlyWorkingHere ? "Present" : new Date(experience.dateEnded).toDateString()}
           </p>
 
-          <ul className='list-disc space-y-5 ml-5 text-lg'>
+          <ul className='list-disc space-y-5 ml-5 text-md md:text-lg'>
             {experience.points.map((point, i) => (
               <li className='md:w-4/5 sm:w-5/6 w-full' key={i}>{point}</li>
             ))}
