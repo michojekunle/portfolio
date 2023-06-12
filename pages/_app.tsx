f087976a5ff3fd6721408e52046f4db659286b7e
AMD <90222287+michojekunle@users.noreply.github.com>
2023-06-12 07:37:50 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,13 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
    <>
       <Head>
-        
+        <title>Michael Ojekunle || @midev</title>
+        <meta name="description" content='Michael Ojekunle || Full Stack Developer || Frontend Engineer' />
+        <meta property="og:title" content='Michael Ojekunle' />
+        <meta property="og:description" content='Learn more about Michael Ojekunle || Full Stack Developer || Frontend Engineer' />
+        <meta property="og:url" content='https://midev.vercel.app' />
+        <meta property="og:type" content="website" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </Head>
     <Component {...pageProps} />
     <Analytics/>
