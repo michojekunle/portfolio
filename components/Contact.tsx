2ac142c66ebe3d8a3ba46a4af125eb40e7b6f1d7
AMD <90222287+michojekunle@users.noreply.github.com>
2023-09-06 15:29:46 +0100

    made changes

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -21,41 +21,43 @@ function Contact({ pageInfo }: Props) {
   }
 
   return (
-    <div className='min-h-screen relative flex flex-col text-center md:text-left max-w-7xl w-full md:flex-row px-10 justify-evenly mx-auto items-center'>
-        <h3 className=' uppercase tracking-[15px] md:tracking-[20px] text-gray-500 text-2xl text-center'>
-          contact
-        </h3>
-        <div className="flex flex-col space-y-6 md:space-y-10 mt-0">
-          <h4 className='text-lg sm:text-xl md:text-4xl font-semibold text-center '>
-            I have got just what you need&nbsp;
-            <span className='underline decoration-[#f7A52a]/50 '>Lets Talk</span>
-          </h4>
-          <div className='flex flex-col space-y-7 md:space-y-10 items-center justify-center'>
-            <div className='flex items-center space-x-5'>
-              <PhoneIcon className='text-[#f7A52a] w-5 h-5 md:h-7 md:w-7 animate-pulse'/>
-              <p className='text-md sm:text-lg md:text-2xl'>{pageInfo.phoneNumber}</p>
-            </div>
+    <div className='w-full'>
+      <h3 className=' uppercase tracking-[15px] md:tracking-[20px] text-gray-500 text-2xl text-center'>
+        contact
+      </h3>
+      <div className='min-h-screen relative flex flex-col text-center md:text-left max-w-7xl w-full md:flex-row px-10 justify-evenly mx-auto items-center'>
+          <div className="flex flex-col space-y-6 md:space-y-10 mt-0">
+            <h4 className='text-lg sm:text-xl md:text-4xl font-semibold text-center '>
+              I have got just what you need&nbsp;
+              <span className='underline decoration-[#f7A52a]/50 '>Lets Talk</span>
+            </h4>
+            <div className='flex flex-col space-y-7 md:space-y-10 items-center justify-center'>
+              <div className='flex items-center space-x-5'>
+                <PhoneIcon className='text-[#f7A52a] w-5 h-5 md:h-7 md:w-7 animate-pulse'/>
+                <p className='text-md sm:text-lg md:text-2xl'>{pageInfo.phoneNumber}</p>
+              </div>
 
-            <div className='flex items-center space-x-5'>
-              <EnvelopeIcon className='text-[#f7A52a] w-5 h-5 md:h-7 md:w-7 animate-pulse'/>
-              <p className='text-md sm:text-lg md:text-2xl'>{pageInfo.email}</p>
-            </div>
+              <div className='flex items-center space-x-5'>
+                <EnvelopeIcon className='text-[#f7A52a] w-5 h-5 md:h-7 md:w-7 animate-pulse'/>
+                <p className='text-md sm:text-lg md:text-2xl'>{pageInfo.email}</p>
+              </div>
 
-            <div className='flex items-center space-x-5'>
-              <MapPinIcon className='text-[#f7A52a] w-5 h-5 md:h-7 md:w-7 animate-pulse'/>
-              <p className='text-md sm:text-lg md:text-2xl'>{pageInfo.address}</p>
+              <div className='flex items-center space-x-5'>
+                <MapPinIcon className='text-[#f7A52a] w-5 h-5 md:h-7 md:w-7 animate-pulse'/>
+                <p className='text-md sm:text-lg md:text-2xl'>{pageInfo.address}</p>
+              </div>
             </div>
+            <form className='flex flex-col space-y-2 w-full mx-auto' onSubmit={handleSubmit(onSubmit)}>
+              <div className='flex space-x-2 '>
+                <input {...register('name')} className='contactInput' placeholder='Name' type="text" />
+                <input {...register('email')} className='contactInput' placeholder='Email' type="email" />
+              </div>
+              <input {...register('subject')} className='contactInput' placeholder='Subject' type="text" />
+              <textarea {...register('message')} className='contactInput' placeholder='Message'/>
+              <button type='submit' className='text-md md:text-lg py-2 px-4 md:py-4 md:px-8 rounded-md text-black font-bold bg-[#f7A52a]'>Submit</button>
+            </form>
           </div>
-          <form className='flex flex-col space-y-2 w-full mx-auto' onSubmit={handleSubmit(onSubmit)}>
-            <div className='flex space-x-2 '>
-              <input {...register('name')} className='contactInput' placeholder='Name' type="text" />
-              <input {...register('email')} className='contactInput' placeholder='Email' type="email" />
-            </div>
-            <input {...register('subject')} className='contactInput' placeholder='Subject' type="text" />
-            <textarea {...register('message')} className='contactInput' placeholder='Message'/>
-            <button type='submit' className='text-md md:text-lg py-2 px-4 md:py-4 md:px-8 rounded-md text-black font-bold bg-[#f7A52a]'>Submit</button>
-          </form>
-        </div>
+      </div>
     </div>
   )
 }
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -13,8 +13,8 @@ const Skills = ({ skills }: Props) => {
       <h3 className='uppercase tracking-[15px] md:tracking-[20px] text-gray-500 text-2xl text-center'>
         skills
       </h3>
+      <h3 className='mt-5 uppercase tracking-[3px] text-gray-500 text-sm text-center'>Hover over a skill for current proficiency</h3>    
       <motion.div className='mt-11 relative flex flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 items-center'>
-        <h3 className='mt-5 uppercase tracking-[3px] text-gray-500 text-sm text-center'>Hover over a skill for current proficiency</h3>    
         <div className='flex flex-wrap gap-3 md:gap-5 mt-11 w-full md:w-[400px]'>
           {skills.slice(0, skills.length/2).map((skill, i) => (
             <Skill key={i} skill={skill}/>
