741fa0f16629d2c47c2e66f4209ce685cc69a004
AMD <90222287+michojekunle@users.noreply.github.com>
2023-09-06 11:12:47 +0100

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -18,13 +18,13 @@ const Projects = ({ projects }: Props) => {
       transition={{duration: 1.5}}
       className='h-screen relative flex flex-col items-center justify-center'
     >
-      <h3 className='uppercase md:tracking-[20px] text-gray-500 text-2xl text-center'>
+      <h3 className='uppercase tracking-[15px] md:tracking-[20px] text-gray-500 text-2xl text-center'>
         Projects
       </h3>
 
-      <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7A52A]/80'>
+      <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-thin scrollbar-rounded-[12px] scrollbar-track-gray-400/20 scrollbar-thumb-[#F7A52A]/80'>
         {projects.map((project, idx) => (
-          <div key={idx} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-444 h-screen'>
+          <div key={idx} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-11 md:p-20 lg:p-44 h-screen'>
             <motion.div
               initial={{
                 y: -300,
@@ -46,16 +46,16 @@ const Projects = ({ projects }: Props) => {
               />
             </motion.div>
             <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
-              <h4 className='text-xl sm:text-2xl md:text-4xl font-semibold'>
-                <a href={project.linkToBuild} className='underline decoration-[#F7A52A]/50'>Case Study {idx + 1} of {projects.length}</a>: {project?.title} 
+              <h4 className='text-xl sm:text-2xl md:text-4xl font-semibold w-full'>
+                <a href={project.linkToBuild} className='underline decoration-[#F7A52A]/50 w-full'>Case Study {idx + 1} of {projects.length}</a>: {project?.title} 
               </h4>
               <div className='flex items-center space-x-2 justify-center'>
                 {project?.technologies.map((technology, i) => (
-                  <Image className='h-10 w-10' key={i} src={technology.image} alt="class" placeholder='blur' layout="responsive"/>
+                  <Image className='h-10 w-10' key={i} src={technology?.image} alt="class" placeholder='blur' layout="responsive"/>
                 ))}
               </div>
 
-              <p className='text-lg text-center md:text-left'>{project?.summary}</p>
+              <p className='text-md opacity-75 md:text-lg text-center md:text-left'>{project?.summary}</p>
             </div>
 
           </div>
